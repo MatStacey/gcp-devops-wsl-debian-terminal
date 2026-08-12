1bac696145e6f42389c32dc534339b825f39f002
mathewstacey <mathew.stacey@rentokil-initial.com>
2026-08-12 18:23:42 +0100

    feat(theme): add colorblind color palette

---
+++ .bash.d/config/themes/colorblind.sh
@@ -0,0 +1,14 @@
+# Okabe-Ito Accessible Color Palette
+export C_RED=$'\e[38;2;213;94;0m'      # Vermillion
+export C_GREEN=$'\e[38;2;0;158;115m'   # Bluish Green
+export C_YELLOW=$'\e[38;2;230;159;0m'  # Orange
+export C_BLUE=$'\e[38;2;0;114;178m'    # Blue
+export C_CYAN=$'\e[38;2;86;180;233m'   # Sky Blue
+export C_WHITE=$'\e[38;2;240;240;240m' # Off-White
+export C_DIM=$'\e[38;2;153;153;153m'   # Gray
+
+export CB_RED=$'\e[1;38;2;213;94;0m'
+export CB_GREEN=$'\e[1;38;2;0;158;115m'
+export CB_YELLOW=$'\e[1;38;2;230;159;0m'
+export CB_BLUE=$'\e[1;38;2;0;114;178m'
+export CB_CYAN=$'\e[1;38;2;86;180;233m'
