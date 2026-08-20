99e9d286c28d816e94bc6758a7a94bab1ee2ea33
mathewstacey <mathew.stacey@rentokil-initial.com>
2026-08-20 13:12:10 +0100

    style: format python scripts with PEP 8 standards

--- .bash.d/lib/python/update_readme_summary.py
+++ .bash.d/lib/python/update_readme_summary.py
@@ -1,5 +1,9 @@
-import sys, re
-with open(sys.argv[1], "r", encoding="utf-8") as f: c = f.read()
+import re
+import sys
+
+with open(sys.argv[1], "r", encoding="utf-8") as f:
+    c = f.read()
 updates = "## 🚀 Recent Updates & Enhancements\n\n" + sys.argv[2] + "\n\n---"
 c_new = re.sub(r"## 🚀 Recent Updates & Enhancements.*?---", updates, c, flags=re.DOTALL)
-with open(sys.argv[1], "w", encoding="utf-8") as f: f.write(c_new)
+with open(sys.argv[1], "w", encoding="utf-8") as f:
+    f.write(c_new)
